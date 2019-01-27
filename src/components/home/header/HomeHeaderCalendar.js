645010a814ae5aac5aec141b632a43afa6fd2bee
Michael Silver <michaelssilver@me.com>
2019-01-27 13:39:19 -0500

    Add react-calendar

---
+++ src/components/home/header/HomeHeaderCalendar.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Calendar from 'react-calendar';
+
+class HomeHeaderCalendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(date) {
+    this.setState({ date });
+  }
+
+  render() {
+    return (
+      <div className="HomeHeaderCalendar">
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
+
+export default HomeHeaderCalendar;
