6d1f973c582d231aa10aca03a0065794faaba639
Jack Cook <hello@jackcook.nyc>
2019-01-27 16:53:37 -0500

    Cleaned up project

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Helmet>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta name="theme-color" content="#5f63d4" />
             <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Nunito:300,400,600,700" rel="stylesheet" />
             <title>Dormspam</title>
           </Helmet>
