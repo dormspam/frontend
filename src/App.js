75ce3e7462edad5e1231d6d233bb0e8b2d51ef37
Jack Cook <hello@jackcook.nyc>
2019-01-27 16:46:19 -0500

    Imported helmet for fonts/other head tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import EventView from "./components/event/EventView";
 import HomeView from "./components/home/HomeView";
@@ -8,10 +9,16 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="DormspamApp">
-          <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Nunito:400,700" rel="stylesheet" />
-          <Route exact path="/" component={HomeView} />
-          <Route exact path="/event/:id" component={EventView} />
+        <div>
+          <Helmet>
+            <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Nunito:300,400,600,700" rel="stylesheet" />
+            <title>Dormspam</title>
+          </Helmet>
+
+          <Switch>
+            <Route exact path="/" component={HomeView} />
+            <Route exact path="/event/:id" component={EventView} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
