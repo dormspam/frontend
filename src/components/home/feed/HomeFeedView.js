330a7b88473e24aab3d1f99fcbbbc6f01d63c752
Jack Cook <hello@jackcook.nyc>
2019-01-27 17:24:55 -0500

    Moved event modal to the right column

--- src/components/home/feed/HomeFeedView.js
+++ src/components/home/feed/HomeFeedView.js
@@ -1,41 +1,15 @@
 import React, { Component } from "react";
 import "./HomeFeedView.css";
 
-import HomeFeedEventModal from "./HomeFeedEventModal";
 import HomeFeedEventsList from "./HomeFeedEventsList";
 import HomeFeedTimelineView from "./HomeFeedTimelineView";
 
 class HomeFeedView extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      event: {
-        id: -1
-      }
-    };
-
-    this.handleSelectEvent = this.handleSelectEvent.bind(this);
-  }
-
-  handleSelectEvent(event) {
-    if (this.state.event.id === event.id) {
-      this.setState({
-        event: {
-          id: -1
-        }
-      });
-    } else {
-      this.setState({ event });
-    }
-  }
-
   render() {
     return (
       <div className="HomeFeedView">
         <HomeFeedTimelineView />
-        <HomeFeedEventsList expanded={this.state.event.id === -1} selectedEvent={this.state.event} onSelectEvent={this.handleSelectEvent} />
-        <HomeFeedEventModal event={this.state.event} expanded={this.state.event.id !== -1} />
+        <HomeFeedEventsList selectedEvent={this.props.selectedEvent} onSelectEvent={this.props.onSelectEvent} />
       </div>
     );
   }
