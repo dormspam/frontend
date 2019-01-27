19fc78b3c9e48bc1e5dd228458542afd65952ed6
Arlene Siswanto <arlenesiswanto@gmail.com>
2019-01-27 16:03:48 -0500

    calendar

--- src/components/home/header/HomeHeaderCalendar.js
+++ src/components/home/header/HomeHeaderCalendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Calendar from 'react-calendar';
+import "./HomeHeaderCalendar.css";
 
 class HomeHeaderCalendar extends Component {
   constructor(props) {
