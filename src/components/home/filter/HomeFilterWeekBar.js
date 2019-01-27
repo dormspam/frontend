678e4da313133a3faf6323e9529d1685030901d3
Michael Silver <michaelssilver@me.com>
2019-01-27 16:01:23 -0500

    Add basic week frontend

--- src/components/home/filter/HomeFilterWeekBar.js
+++ src/components/home/filter/HomeFilterWeekBar.js
@@ -4,12 +4,36 @@ import "./HomeFilterWeekBar.css";
 import HomeFilterWeekBarDayItem from "./HomeFilterWeekBarDayItem";
 
 class HomeFilterWeekBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      day: new Date(),
+    };
+  }
+
   render() {
+    const weekDays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const dayTagsData = weekDays.map(
+      (day, i) => {
+        const date = new Date(this.state.day.getTime() + (24*60*60*1000*(i-this.state.day.getDay()))).getDate();
+        return {
+          day: day,
+          date: date,
+          active: this.state.day.getDate() === date
+        };
+      }
+    );
+    const dayTags = dayTagsData.map(
+      day => <HomeFilterWeekBarDayItem key={day.date} data={day} />
+    );
+
     return (
       <div className="HomeFilterWeekBar">
         <div className="left-button" />
         <div className="weekdays">
-          <HomeFilterWeekBarDayItem />
+          {/* <HomeFilterWeekBarDayItem day='S' active={true}/> */}
+          {dayTags}
         </div>
         <div className="right-button" />
       </div>
--- src/components/home/filter/HomeFilterWeekBarDayItem.js
+++ src/components/home/filter/HomeFilterWeekBarDayItem.js
@@ -4,10 +4,10 @@ import "./HomeFilterWeekBarDayItem.css";
 class HomeFilterWeekBarDayItem extends Component {
   render() {
     return (
-      <div className="HomeFilterWeekBarDayItem">
-        <p className="weekday">S</p>
+      <div className={"HomeFilterWeekBarDayItem" + (this.props.data.active ? " active" : "")}>
+        <p className="weekday">{this.props.data.day}</p>
         <div className="circle">
-          <p className="day">5</p>
+          <p className="day">{this.props.data.date}</p>
         </div>
       </div>
     );
