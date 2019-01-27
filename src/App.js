5e974b763986b5a2d87660f3981ddc3d9cf64dd1
Jack Cook <hello@jackcook.nyc>
2019-01-27 11:44:44 -0500

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import EventView from "./components/event/EventView";
+import HomeView from "./components/home/HomeView";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="DormspamApp">
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/event/:id" component={EventView} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/event/EventView.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EventView extends Component {
+  render() {
+    return (
+      <div className="EventView">
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+
+export default EventView;
