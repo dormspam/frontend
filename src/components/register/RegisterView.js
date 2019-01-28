ed410f86c6036fde043fb0e08841ef563463b828
Jack Cook <hello@jackcook.nyc>
2019-01-28 11:36:35 -0500

    Added settings button

---
+++ src/components/register/RegisterView.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RegisterView extends Component {
+  render() {
+    return (
+      <div className="RegisterView">
+        
+      </div>
+    )
+  }
+}
+
+export default RegisterView;
