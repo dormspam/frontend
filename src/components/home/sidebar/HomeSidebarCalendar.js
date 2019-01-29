885be8d788106bca79d6fdcfe8925ce36c8c5f91
Michael Silver <michaelssilver@me.com>
2019-01-28 19:14:11 -0500

    Add calendar frontend

--- src/components/home/sidebar/HomeSidebarCalendar.js
+++ src/components/home/sidebar/HomeSidebarCalendar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import moment from "moment";
 import "./HomeSidebarCalendar.css";
 
 import HomeSidebarCalendarDayItem from "./HomeSidebarCalendarDayItem";
@@ -8,61 +9,48 @@ class HomeSidebarCalendar extends Component {
     super(props);
 
     this.state = {
-      day: new Date(),
+      today: moment(),
     };
   }
 
   render() {
+    let dayTagsData = [];
+    const m = moment(this.state.today);
+
+    const monthStart = m.startOf("month");
+    let monthEnd = moment(monthStart);
+    monthEnd = monthEnd.endOf("month");
+
+    const weekStart = monthStart.startOf("week");
+    const weekEnd = monthEnd.endOf("week");
+
+    for (let m = weekStart; m.isBefore(weekEnd); m.add(1, 'days')) {
+      dayTagsData.push({
+        date: m.date(),
+        active: m.isSame(this.state.today, 'day'),
+        focus: m.isSame(this.state.today, 'month'),
+      });
+    }
+
+    const dayTags = dayTagsData.map(
+      (day, i) => <HomeSidebarCalendarDayItem
+                    key={i}
+                    day={day.date}
+                    active={day.active}
+                    focus={day.focus}
+                  />
+    );
+
     return (
       <div className="HomeSidebarCalendar">
         <div className="header">
           <div className="left-button" />
-          <h2>February</h2>
+          <h2>{this.state.today.format('MMMM')}</h2>
           <div className="right-button" />
         </div>
         <div className="calendar">
-
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
-          <HomeSidebarCalendarDayItem />
+          {dayTags}
         </div>
-
       </div>
     );
   }
