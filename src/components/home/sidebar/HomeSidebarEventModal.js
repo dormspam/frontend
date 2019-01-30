43a22969cbcb49ca07eba20a7ad8cab860d7be83
Jack Cook <hello@jackcook.nyc>
2019-01-29 21:16:30 -0500

    Fixed image hiding condition

--- src/components/home/sidebar/HomeSidebarEventModal.js
+++ src/components/home/sidebar/HomeSidebarEventModal.js
@@ -5,7 +5,7 @@ class HomeSidebarEventModal extends Component {
   render() {
     return (
       <div className={"HomeSidebarEventModal" + (this.props.event.id === -1 ? " hidden" : "")}>
-        <img className={this.props.event.image === "" ? " hidden" : ""} src={this.props.event.image} alt="Event promotional material" />
+        <img className={this.props.event.image === undefined ? " hidden" : ""} src={this.props.event.image} alt="Event promotional material" />
         <h2>{this.props.event.title}</h2>
         <p>{this.props.event.description}</p>
       </div>
