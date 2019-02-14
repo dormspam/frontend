38779a147f8e5b085e9091ff1aed3861737c76cc
Arlene Siswanto <arlenesiswanto@gmail.com>
2019-02-14 00:16:14 -0500

    tags, description text, author

--- src/components/home/sidebar/HomeSidebarEventModal.js
+++ src/components/home/sidebar/HomeSidebarEventModal.js
@@ -3,12 +3,24 @@ import "./HomeSidebarEventModal.css";
 
 class HomeSidebarEventModal extends Component {
   render() {
+    let categories = ["Tech", "Food"];
     if (this.props.event === null) {
       return <div />;
     }
 
     return (
       <div className="HomeSidebarEventModal">
+        <div className="metadata-top">
+          <h3>{this.props.event.sent_from}</h3>
+          <div className="minipadding"></div>
+          <h3>
+            <span className="tags">&#9679; Tech</span>
+            <span className="tags">&#9679; Art</span>
+          </h3>
+          <div className="padding"></div>
+          <hr />
+        </div>
+
         <h2>{this.props.event.title}</h2>
         <div dangerouslySetInnerHTML={{__html: this.props.event.description}} />
       </div>
