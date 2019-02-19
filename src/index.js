ac21fda8e1f33bed77329c52e4f98ea555bcfbf8
Jack Cook <hello@jackcook.nyc>
2019-02-18 20:59:11 -0500

    oops

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ axios.get(process.env.REACT_APP_BACKEND_URL + "/users/current", {
   ReactDOM.render(<LoginView />, document.getElementById("root"));
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
