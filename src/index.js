bea85150c70e1481050c0708ab7dcd95929ba536
Jack Cook <hello@jackcook.nyc>
2019-02-18 20:55:25 -0500

    Added google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-134713779-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
