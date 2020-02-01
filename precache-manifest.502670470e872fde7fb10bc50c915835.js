72373425599f7d2fba74b1a94064bff1d0ed4efc
Kevin Fang <reddacc@gmail.com>
2020-02-01 17:45:14 -0500

    Updates

---
+++ precache-manifest.502670470e872fde7fb10bc50c915835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a2d943d02f5c40fb3d7b5bc01209ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b209463a4601b7037337",
+    "url": "/static/css/main.db89b560.chunk.css"
+  },
+  {
+    "revision": "e039347b09eed2208703",
+    "url": "/static/js/2.16e6fa76.chunk.js"
+  },
+  {
+    "revision": "ecadeb4f7b80a54090688f4da9446cac",
+    "url": "/static/js/2.16e6fa76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b209463a4601b7037337",
+    "url": "/static/js/main.89fe91fa.chunk.js"
+  },
+  {
+    "revision": "033388264ef1e06a653c",
+    "url": "/static/js/runtime-main.69ae249f.js"
+  }
+]);
