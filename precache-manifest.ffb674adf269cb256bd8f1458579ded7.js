6f371ee95adcdfd8ed06e897c630cfb28fa76a75
Justin Yu <justinyu1618@gmail.com>
2020-02-03 00:12:07 -0500

    Updates

---
+++ precache-manifest.ffb674adf269cb256bd8f1458579ded7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81294ad07b3bcd6f37bf3ce0257457a",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "43fb3c58c96ac56fc6c6",
+    "url": "/frontend/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "84897231570ccda7fffa",
+    "url": "/frontend/static/css/main.ed0785fd.chunk.css"
+  },
+  {
+    "revision": "43fb3c58c96ac56fc6c6",
+    "url": "/frontend/static/js/2.417748ed.chunk.js"
+  },
+  {
+    "revision": "026b41b17b3048518fdd0ed8bb9ed94d",
+    "url": "/frontend/static/js/2.417748ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84897231570ccda7fffa",
+    "url": "/frontend/static/js/main.9cd31ccb.chunk.js"
+  },
+  {
+    "revision": "5a3179188274db41fbb9",
+    "url": "/frontend/static/js/runtime-main.1804f944.js"
+  }
+]);
