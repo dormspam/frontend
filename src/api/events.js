141d83dc4d26d59ec9e14418bd71398a55491448
neil-argus <neild@mit.edu>
2023-04-27 17:51:51 -0400

    cleaned up codebase + updated backend integration

--- src/api/events.js
+++ src/api/events.js
@@ -2,24 +2,50 @@ import Request from "./request";
 
 class Events {
   static getEventFrequencyByDate(date) {
-    return new Request("/events/frequency/" + date);
-  }
-
-  static getEventsByDate(date) {
-    return new Request("/events/" + date);
+    return new Request("https://dormdigest.xvm.mit.edu:8432/events/frequency/" + date);
   }
 
   static getEventsByQuery(query) {
-    return new Request("/events?q=" + query);
+    return new Request("https://dormdigest.xvm.mit.edu:8432/events?q=" + query);
   }
 
   static getEventById(id) {
-    return new Request("/events/" + id);
+    return new Request("https://dormdigest.xvm.mit.edu:8432/events/" + id);
   }
 
-  static getAllEvents(){
-    return new Request("/events/all")
+  static getAllEvents() {
+    return new Request("https://dormdigest.xvm.mit.edu:8432/events/all");
   }
+
+  static getEventsByDate(formattedDate) {
+    const [year, month, day] = formattedDate.split('-').map(Number);
+    console.log(year, month);
+  
+    return fetch("https://dormdigest.xvm.mit.edu:8432/get_events_by_month", {
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        month: month,
+        year: year
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Error:', response.status, response.statusText);
+        throw new Error('Error fetching data');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      throw error;
+    });
+  }
+  
 }
 
 export default Events;
