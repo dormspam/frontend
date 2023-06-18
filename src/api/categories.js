71f06213263038b520b363622b65a48ec4b4d333
Huy Dai <huydai@mit.edu>
2023-06-17 20:10:23 -0400

    Add categories definition

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,8 +1,21 @@
 import Request from "./request";
 
+
+const CATEGORIES_COLORS = {
+  'OTHER': "#888888",
+  'FOOD': "#EE6F6F",
+  'CAREER': "#459AF6",
+  'FUNDRAISING':"#A16EE5",
+  'APPLICATION':"#459AF6",
+  'PERFORMANCE':"#12DAA4",
+  'BOBA':"#F6B957",
+  'TALKS':"#73F23A",
+  'SOCIAL':"#25C8D3"
+}
+
 class Categories {
   static getCategories() {
-    return new Request("/categories");
+    return CATEGORIES_COLORS;
   }
 }
 
