ab92db751522afeb43ec794b3b6d8c7b623f7db5
Huy Dai <huydai@mit.edu>
2023-08-06 00:20:23 -0400

    Uncomment Github page stuff

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactGA.initialize("UA-38208507-9");
-ReactGA.pageview(window.location.pathname + window.location.search);
+//ReactGA.initialize("UA-38208507-9");
+//ReactGA.pageview(window.location.pathname + window.location.search);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
